ba89e23bab76905c47566598f42681e20a394852
khaledGuedria <khaled.guedria.1@esprit.tn>
2023-06-22 12:56:24 +0100

    Screens setup blank

---
+++ lib/presentation/cart_screen/cart_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CartScreeen extends StatelessWidget {
+  const CartScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
