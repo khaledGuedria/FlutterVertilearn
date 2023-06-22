90c701f24c87f1e328101138c36ecf6f4d84e125
khaledGuedria <khaled.guedria.1@esprit.tn>
2023-06-22 12:46:32 +0100

    Day3: Full exemple app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:day3_exemple/utilities/providers.dart';
+import 'package:day3_exemple/utilities/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(MultiBlocProvider(providers: providers, child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Day 3 : Exemple',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: routes,
+    );
+  }
+}
